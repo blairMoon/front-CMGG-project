08ce98a84f57e771720062062370998bf2861693
오수빈 <catherine621@naver.com>
2023-04-10 15:39:16 +0900

    feature(WholeLectures):api작업중

--- src/pages/WholeLecuturePage/WholeLecturesPage.tsx
+++ src/pages/WholeLecuturePage/WholeLecturesPage.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 import LectureCard from "./LectureCard.tsx/LectureCard";
-
+import { useNavigate, useParams, useLocation } from "react-router-dom";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 interface Props {}
 
 const WholeLecturePage: React.FC<Props> = (props: Props) => {
+  type RouteParams = {
+    bigCategory: string;
+    smallCategory: string;
+  };
+  const { bigCategory, smallCategory } = useParams<RouteParams>();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const pageNum = params.get("page");
+  const searchName = params.get("search");
+  const {
+    isLoading: isSearching,
+    data,
+    refetch: refetchSearch,
+  } = useQuery(
+    ["lectureSearch", bigCategory, smallCategory, pageNum, searchName],
+    getLectureAndCategoryAndSearch
+  );
   return (
     <>
       <div>WholeLecturePage</div>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,13 @@
 import Cookies from "js-cookie";
 import axios from "axios";
+
 // const value: string | undefined = Cookies.get("my-cookie");
 const instance = axios.create({
   baseURL: `..`,
   withCredentials: false,
 });
 
+export type QueryKey = [string, string, string, number?, string?];
 export const getAllCoins = () =>
   instance.get("coins").then((res) => res.data.slice(0, 100));
 
@@ -17,3 +19,23 @@ export const instanceNotLogin = axios.create({
 
   withCredentials: true,
 });
+
+export const getLectureAndCategoryAndSearch = async ({
+  queryKey,
+}: {
+  queryKey: QueryKey;
+}) => {
+  const [, bigCategory, smallCategory, page = 1, searchName] = queryKey;
+
+  if (searchName) {
+    return await instanceNotLogin
+      .get(
+        `lectures/${bigCategory}/${smallCategory}/?page=${page}&search=${searchName}`
+      )
+      .then((res) => res.data);
+  } else {
+    return await instanceNotLogin
+      .get(`lectures/${bigCategory}/${smallCategory}/?page=${page}`)
+      .then((res) => res.data);
+  }
+};
