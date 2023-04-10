12215300f68a5c65c02d3a001e6b5fdb0f918373
khs <hyeongs2323@gmail.com>
2023-04-10 14:33:14 +0900

    feat(MyCart): cart layout

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,27 @@ import App from "./App";
 import ReactDOM from "react-dom/client";
 import * as Sentry from "@sentry/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "../node_modules/scss-reset/src/scss/_reset.scss";
 
 Sentry.init({
-  dsn: process.env.DSN,
+  dsn: "https://97f36cbf38094c0381c09337fd4bca6c@o4504964748345344.ingest.sentry.io/4504967641759744",
   environment: "development",
   integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
   tracesSampleRate: 1.0,
 });
 
-const client = new QueryClient();
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={client}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>
-);
+try {
+  const client = new QueryClient();
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={client}>
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>
+  );
+} catch (error) {
+  Sentry.captureException(error);
+}
