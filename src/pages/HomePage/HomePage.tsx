74276fc6c62c705478988eab88aa544a5ac9e3e5
오수빈 <catherine621@naver.com>
2023-04-12 11:04:37 +0900

    fix(wholeLectures):router수정중

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomePage(): React.ReactElement {
   return (
     <>
       <div>home</div>
+      <Link to="wholeLectures/all/all">전체강의</Link>
     </>
   );
 }
