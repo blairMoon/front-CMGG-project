19f762fee37b5df97da2efc5e6916190e01a8161
khs <hyeongs2323@gmail.com>
2023-04-18 10:56:17 +0900

    feat(page): LectureRegister Dark Scss Setting

--- src/components/Radio/MyRadio.tsx
+++ src/components/Radio/MyRadio.tsx
@@ -1,5 +1,5 @@
 import { forwardRef } from "react";
-import { Radio, RadioProps } from "@chakra-ui/react";
+import { Radio, RadioProps, useColorMode } from "@chakra-ui/react";
 
 interface MyRadioProps extends RadioProps {
   testId: string;
@@ -7,9 +7,18 @@ interface MyRadioProps extends RadioProps {
 
 export const MyRadio = forwardRef<HTMLInputElement, MyRadioProps>(
   ({ testId, children, ...props }, ref) => {
+    const { colorMode, toggleColorMode } = useColorMode();
+
     return (
       <div data-testid={testId} style={{ width: "calc(100% / 4)" }}>
-        <Radio {...props} ref={ref} w="100%" borderColor={"blackAlpha.600"}>
+        <Radio
+          {...props}
+          ref={ref}
+          w="100%"
+          borderColor={
+            colorMode === "light" ? "blackAlpha.600" : "whiteAlpha.600"
+          }
+        >
           {children}
         </Radio>
       </div>
